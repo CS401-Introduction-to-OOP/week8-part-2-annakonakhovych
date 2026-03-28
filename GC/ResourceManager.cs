de94a1b89af25837b1c9928eff765a276d58550a
annakonakhovych <anna.konakhovych.25@kse.org.ua>
2026-03-28 16:01:24 +0200

    Stage 1.3

--- GC/ResourceManager.cs
+++ GC/ResourceManager.cs
@@ -1,6 +1,29 @@
 ﻿namespace GC;
 
-public class ResourceManager
+public class ResourceManager<T> where T : Resource
 {
+    private List<T> _resource = new List<T>();
+
+    public void Add(T resource)
+    {
+        _resource.Add(resource);
+    }
+
+    public void OpenAll()
+    {
+        foreach (var resource in _resource)
+        {
+            resource.Open();
+        }
+    }
+    
+    public void CloseAll()
+    {
+        foreach (var resource in _resource)
+        {
+            resource.Close();
+        }
+    }
+    
     
 }
