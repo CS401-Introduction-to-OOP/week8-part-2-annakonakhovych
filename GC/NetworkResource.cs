47c5447cd07262538f19d6691b6edd598b93a3c6
annakonakhovych <anna.konakhovych.25@kse.org.ua>
2026-03-28 15:40:10 +0200

    Stage 1.1

---
+++ GC/NetworkResource.cs
@@ -0,0 +1,6 @@
+﻿namespace GC;
+
+public class NetworkResource
+{
+    
+}
---
+++ GC/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace GC;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
